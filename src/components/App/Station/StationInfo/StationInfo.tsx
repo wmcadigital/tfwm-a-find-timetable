b3bd494333bb41f3835747b1faa6c3b683083280
houbly <h.blyden@gmail.com>
2021-10-29 09:12:36 +0100

    add station map

--- src/components/App/Station/StationInfo/StationInfo.tsx
+++ src/components/App/Station/StationInfo/StationInfo.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 // Components
 import { useStationContext } from 'globalState';
 import Button from 'components/shared/Button/Button';
 import Icon from 'components/shared/Icon/Icon';
 import s from '../Stop.module.scss';
 import TrainDepartures from '../TrainDepartures/TrainDepartures';
+import Map from '../Map/Map';
 
 const Parking = () => {
   return (
@@ -58,29 +60,36 @@ const Facilities = () => {
 };
 
 const StopInfo = () => {
+  const [showMap, setShowMap] = useState(false);
   const [{ stationPoint }] = useStationContext();
   const station = stationPoint.data[0];
   return (
-    <div className="wmnds-col-md-2-3">
-      <div className="wmnds-grid wmnds-grid--spacing-2-md wmnds-grid--justify-between">
-        <div className="wmnds-col-2-3 wmnds-m-b-lg">
-          <h2>
-            {station.name}{' '}
-            <Icon className={`${s.modeIcon} ${s.train}`} iconName="modes-isolated-rail" />
-          </h2>
-          <h3 className="wmnds-m-t-none">Rail zone 1</h3>
-        </div>
-        <div className="wmnds-col-auto">
-          <Button
-            btnClass="wmnds-btn--secondary wmnds-col-1"
-            text="View map"
-            iconRight="general-location-pin"
-          />
+    <div>
+      <div className="wmnds-col-md-2-3">
+        <div className="wmnds-grid wmnds-grid--spacing-2-md wmnds-grid--justify-between">
+          <div className="wmnds-col-2-3 wmnds-m-b-lg">
+            <h2>
+              {station.name}{' '}
+              <Icon className={`${s.modeIcon} ${s.train}`} iconName="modes-isolated-rail" />
+            </h2>
+            <h3 className="wmnds-m-t-none">Rail zone 1</h3>
+          </div>
+          <div className="wmnds-col-auto">
+            <Button
+              btnClass="wmnds-btn--secondary wmnds-col-1"
+              text={showMap ? 'Hide map' : 'View map'}
+              iconRight="general-location-pin"
+              onClick={() => setShowMap(!showMap)}
+            />
+          </div>
         </div>
       </div>
-      <TrainDepartures />
-      <Facilities />
-      <Parking />
+      {showMap && <Map />}
+      <div className="wmnds-col-md-2-3">
+        <TrainDepartures />
+        <Facilities />
+        <Parking />
+      </div>
     </div>
   );
 };
