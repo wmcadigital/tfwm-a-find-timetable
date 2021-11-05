75b6799206e2d761fa0c7146f65b60741e2bdad1
houbly <h.blyden@gmail.com>
2021-11-05 10:41:12 +0000

    add help text

--- src/components/App/FindStopStation/MapView/MapView.tsx
+++ src/components/App/FindStopStation/MapView/MapView.tsx
@@ -12,16 +12,34 @@ const SelectedStop = () => {
 
   if (selectedStop) {
     return (
-      <SearchResult
-        stopType={selectedStop.properties.type}
-        distance={`${selectedStop.locationDistance?.toFixed(1)} miles away`}
-        text={selectedStop.properties.name}
-        atcoCode={selectedStop.properties.atcoCode || selectedStop.properties.crs || ''}
-        key={selectedStop.properties.atcoCode}
-      />
+      <div className={s.results}>
+        <SearchResult
+          stopType={selectedStop.properties.type}
+          distance={`${selectedStop.locationDistance?.toFixed(1)} miles away`}
+          text={selectedStop.properties.name}
+          atcoCode={selectedStop.properties.atcoCode || selectedStop.properties.crs || ''}
+          key={selectedStop.properties.atcoCode}
+        />
+      </div>
     );
   }
   return null;
+};
+
+const HelpMessage = () => {
+  const [{ selectedModes, selectedStopId, stops, location }] = useStopStationContext();
+  let helpMsg: any = null;
+  if (location && selectedModes.length === 0) {
+    helpMsg = <>Select modes of transport to view stops and stations near {location.name}</>;
+  } else if (!stops.length && location && selectedModes.length) {
+    helpMsg = <>No stops or stations were found. Please try increasing the search radius.</>;
+  } else if (!selectedStopId && stops.length) {
+    helpMsg = <>Select a stop{selectedModes.includes('rail') && ' or station'} on the map</>;
+  }
+  if (!helpMsg) {
+    return null;
+  }
+  return <div className="wmnds-m-t-lg wmnds-msg-help">{helpMsg}</div>;
 };
 
 const MapView = () => {
@@ -31,11 +49,8 @@ const MapView = () => {
         <div className="wmnds-col-1-1 wmnds-col-md-1-2 wmnds-col-lg-1-3">
           <div className="wmnds-p-md">
             <StopStationSearch />
-            {SelectedStop && (
-              <div className={s.results}>
-                <SelectedStop />
-              </div>
-            )}
+            <HelpMessage />
+            <SelectedStop />
           </div>
         </div>
         <div className="wmnds-col-1-1 wmnds-col-md-1-2 wmnds-col-lg-2-3">
